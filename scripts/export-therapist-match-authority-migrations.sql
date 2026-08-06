ff5071b8bbbcbe12858240a3603616a4b23a88e0
LPredmore <124374222+LPredmore@users.noreply.github.com>
2026-08-06 16:22:18 -0500

    Add exact production migration export query

---
+++ scripts/export-therapist-match-authority-migrations.sql
@@ -0,0 +1,26 @@
+\pset tuples_only on
+\pset format unaligned
+\pset fieldsep ''
+
+select format(
+  E'-- BEGIN MIGRATION %s_%s.sql\n%s\n-- END MIGRATION %s_%s.sql\n',
+  version,
+  name,
+  statements[1],
+  version,
+  name
+)
+from supabase_migrations.schema_migrations
+where version in (
+  '20260806203440',
+  '20260806203612',
+  '20260806203727',
+  '20260806203818',
+  '20260806205541',
+  '20260806205616',
+  '20260806210703',
+  '20260806211144',
+  '20260806211404',
+  '20260806211506'
+)
+order by version;
